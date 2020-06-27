dbc1e1333596d5dfafbeedf323264a030d728079
CShell <shell.calvin7@gmail.com>
2020-06-26 21:25:42 -0600

    Updates

---
+++ precache-manifest.3e1d700baddbd6ecffc83904c545bff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d8b56c320d65d7fd81afe0d53d419f",
+    "url": "/sample-obd-data/index.html"
+  },
+  {
+    "revision": "9da900f275bab46f4bfa",
+    "url": "/sample-obd-data/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6482cb938bfde9e46521",
+    "url": "/sample-obd-data/static/js/2.99e6b756.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sample-obd-data/static/js/2.99e6b756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9da900f275bab46f4bfa",
+    "url": "/sample-obd-data/static/js/main.970f9196.chunk.js"
+  },
+  {
+    "revision": "8fde5d325c3d32b3109a",
+    "url": "/sample-obd-data/static/js/runtime-main.e04fe882.js"
+  }
+]);
